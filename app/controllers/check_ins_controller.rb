4beca34b4a4fca11e2b9543278926b93c55d24a1
Fiona Wang <luna621@gmail.com>
2010-09-11 16:01:06 -1000

    show check in

--- app/controllers/check_ins_controller.rb
+++ app/controllers/check_ins_controller.rb
@@ -1,7 +1,17 @@
 class CheckInsController < ApplicationController
-
+  
   def new
-    @check_in = Location.find(params[:location_id]).check_ins.build
+    @location = Location.find(params[:location_id])
+    @check_in = @location.check_ins.build
   end
-
+  
+  def create
+    @location = Location.find(params[:location_id])
+    @check_in = @location.check_ins.create(params[:check_in])
+    redirect_to location_check_in_path(@location, @check_in)
+  end
+  
+  def show
+    @check_in = CheckIn.find(params[:id])
+  end
 end
